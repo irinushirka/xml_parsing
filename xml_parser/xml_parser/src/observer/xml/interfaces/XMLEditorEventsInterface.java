4d5fe43a7dd654b0587a6eee418cfa400f2610fc
irinushirka <57564030+irinushirka@users.noreply.github.com>
2020-12-01 19:34:10 +0300

    Revert "Fixed file paths"

--- xml_parser/xml_parser/src/observer/xml/interfaces/XMLEditorEventsInterface.java
+++
@@ -1,8 +0,0 @@
-package observer.xml.interfaces;
-import observer.xml.enums.XMLEventType;
-
-public interface XMLEditorEventsInterface {
-    void startXMLParse(String parseFileName);
-    void endXMLParse(String parseFileName);
-    void subscribeTo(XMLEventType event, XMLEventListenerInterface eventListener);
-}
--- xml_parser/xml_parser/src/parser/interfaces/SimpleSAXInterface.java
+++
@@ -1,7 +0,0 @@
-package parser.interfaces;
-
-import java.util.ArrayList;
-
-public interface SimpleSAXInterface {
-    ArrayList<String> start(String tag, String file_name);
-}
